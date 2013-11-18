4aaf06440c9d2d1bd5b0b26b5681841f482ef3bb
Anders Berg <anders.berg@lsi.com>
2013-11-18 23:48:56 +0100

    Proxy script

---
+++ proxy.sh
@@ -0,0 +1,15 @@
+if [ "$1" == "off" ]
+then
+   unset http_proxy
+   unset https_proxy
+   echo "Proxy disabled"
+else
+   case "$1" in
+      pa*) http_proxy="http://palzproxy1.lsi.com:9480" ;;
+      ge*) http_proxy="http://geezproxy1.lsi.com:9400" ;;
+      *)   http_proxy="http://enbzproxy1.lsi.com:9400" ;;
+   esac
+   export http_proxy
+   export https_proxy=$http_proxy
+   echo "Proxy $http_proxy enabled"
+fi
