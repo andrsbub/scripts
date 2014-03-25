564b7d0fcaa335da6392f0a58aa9a1cf76722a65
Anders Berg <anders.berg@lsi.com>
2014-03-25 21:44:29 +0100

    Embedded dts for building images

--- mkfit.sh
+++ mkfit.sh
@@ -1,13 +1,83 @@
 #!/bin/bash
+LOADADDR=0x408000
+if [ "$1" == "--load" -a ! -z "$2" ]; then
+  LOADADDR=$2
+  echo "Using load-addr ${LOADADDR}"
+  shift 2
+fi
 DTB=$1
 if [ -z "$DTB" ]; then
   DTB=axm55xx
 fi
-if [ ! -r ./arch/arm/boot/dts/${DTB}.dts ]; then
+if [ -r ./arch/arm/boot/dts/${DTB}.dtb ]; then
+  echo "Using prebuilt DTB"
+  cp ./arch/arm/boot/dts/${DTB}.dtb arch/arm/boot/current.dtb
+elif [ ! -r ./arch/arm/boot/dts/${DTB}.dts ]; then
   echo "${DTB}.dts: Not found"
   exit 1
+else
+  echo "Building other DTB"
+  ./scripts/dtc/dtc -O dtb -R 32 -p 0x400 -o arch/arm/boot/current.dtb arch/arm/boot/dts/${DTB}.dts
 fi
-./scripts/dtc/dtc -O dtb -R 32 -p 0x400 -o arch/arm/boot/current.dtb arch/arm/boot/dts/${DTB}.dts
 [ $? -eq 0 ] || exit $?
+
+if [ ! -r arch/arm/boot/Image ];then
+  echo "No Image found"
+  exit 1
+fi
+
 cat arch/arm/boot/Image | gzip -c > arch/arm/boot/vmlinux.bin.gz
-mkimage -f kernel_fdt.its linux.fit
+
+TMPFILE=kernel-fdt-${USER}.$$
+cat >$TMPFILE <<-EOF
+/dts-v1/;
+
+/ {
+	description = "Simple image with single Linux kernel and FDT blob";
+	#address-cells = <1>;
+
+	images {
+		kernel@1 {
+			description = "Linux kernel";
+			data = /incbin/("./arch/arm/boot/vmlinux.bin.gz");
+			type = "kernel";
+			arch = "arm";
+			os = "linux";
+			compression = "gzip";
+			load = <${LOADADDR}>;
+			entry = <${LOADADDR}>;
+			hash@1 {
+				algo = "crc32";
+			};
+			hash@2 {
+				algo = "sha1";
+			};
+		};
+		fdt@1 {
+			description = "Flattened Device Tree blob";
+			data = /incbin/("./arch/arm/boot/current.dtb");
+			type = "flat_dt";
+			arch = "arm";
+			compression = "none";
+			hash@1 {
+				algo = "crc32";
+			};
+			hash@2 {
+				algo = "sha1";
+			};
+		};
+	};
+
+	configurations {
+		default = "conf@1";
+		conf@1 {
+			description = "Boot Linux kernel with FDT blob";
+			kernel = "kernel@1";
+			fdt = "fdt@1";
+		};
+	};
+};
+EOF
+
+mkimage -f $TMPFILE linux.fit
+rm -f $TMPFILE
--- proxy.sh
+++ proxy.sh
@@ -7,7 +7,7 @@ else
    case "$1" in
       pa*) http_proxy="http://palzproxy1.lsi.com:9480" ;;
       ge*) http_proxy="http://geezproxy1.lsi.com:9400" ;;
-      *)   http_proxy="http://enbzproxy1.lsi.com:9400" ;;
+      *)   http_proxy="http://enbzproxy1.lsi.com:9480" ;;
    esac
    export http_proxy
    export https_proxy=$http_proxy
